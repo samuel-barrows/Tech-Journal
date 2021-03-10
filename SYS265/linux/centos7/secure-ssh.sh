169a251db4979280d75b8bdee108eb19e209a856
samuel-barrows <samuel.barrows@mymail.champlain.edu>
2021-03-10 11:03:32 -0500

    added start of script and linux directory

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#secure-ssh.sh
+#author samuel barrows
+#creates a new ssh user using the $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "CODE LOL"
