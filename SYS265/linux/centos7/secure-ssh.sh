d65ce24d0caf1c26bd3c581ed4b79898f182ef84
samuel-barrows <samuel.barrows@mymail.champlain.edu>
2021-03-10 16:46:01 +0000

    updated script

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -4,4 +4,26 @@
 #creates a new ssh user using the $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "CODE LOL"
+
+#test that root is running the script
+if [ $(id -u) -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+username=$1
+
+#function to make a user and give the public key to them
+makeuser(){
+  useradd -m -s /bin/bash $username 
+  mkdir /home/$username/.ssh
+  #appends the public key for our web server into the new users authorized keys
+  cat ../public-keys/id_rsa.pub >> /home/$username/.ssh/authorized_keys
+  chmod 700 /home/$username/.ssh
+  chmod 600 /home/$username/.ssh/authorized_keys
+  chown -R $username:$username /home/$username/.ssh
+}
+
+makeuser
+
+
